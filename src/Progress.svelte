2bc503c4fa22bf69507bc9422354d06ed0a544bf
Shane O'Moore <shaneiadt@gmail.com>
2021-01-03 11:58:46 +0000

    add Progress component

---
+++ src/Progress.svelte
@@ -0,0 +1,31 @@
+<style>
+    .progress-container {
+        background: #fff;
+        border-radius: 5px;
+        cursor: pointer;
+        margin: 1.5rem 1rem;
+        height: 4px;
+        width: 90%;
+    }
+    .progress {
+        background: #242323;
+        border-radius: 5px;
+        height: 100%;
+        width: 66%;
+        transition: width 0.1s linear;
+    }
+    .duration-wrapper {
+        position: relative;
+        top: -25px;
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
+
+<div class="progress-container" id="progress-container">
+    <div class="progress" id="progress" />
+    <div class="duration-wrapper" id="duration-wrapper">
+        <span id="current-time">0:00</span>
+        <span id="duration">2:06</span>
+    </div>
+</div>
