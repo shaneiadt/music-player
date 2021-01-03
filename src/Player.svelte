132f21e2afc57304da3b9162e3e1367209180c9f
Shane O'Moore <shaneiadt@gmail.com>
2021-01-03 11:59:10 +0000

    update Player component & base stylesheet

--- src/Player.svelte
+++ src/Player.svelte
@@ -1,5 +1,10 @@
+<script lang="typescript">
+    import Progress from "./Progress.svelte";
+    import Controls from "./Controls.svelte";
+</script>
+
 <div
-    class="player-container flex flex-col items-center shadow-lg rounded-xl bg-gray-100 w-auto h-auto px-1 px-8 pb-8">
+    class="player-container flex flex-col items-center shadow-xl rounded-xl bg-gray-100 w-auto h-auto px-1 px-8 pb-8">
     <div class="img-container relative -top-25 w-64 h-64">
         <img
             class="h-full object-cover shadow-md rounded-lg w-full"
@@ -8,7 +13,9 @@
     </div>
     <h2 id="title" class="text-xl font-bold">Electric Chill Machine</h2>
     <h4 id="artist" class="text-lg">Jacinto</h4>
-    <audio src="/assets/tracks/jacinto-1.mp3" controls>
+    <audio src="/assets/tracks/jacinto-1.mp3">
         <track kind="captions" />
     </audio>
+    <Progress />
+    <Controls />
 </div>
