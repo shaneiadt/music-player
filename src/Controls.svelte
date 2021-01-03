dd7e930ec0e1722e3523da1f233137f40e993eda
Shane O'Moore <shaneiadt@gmail.com>
2021-01-03 11:58:29 +0000

    add Controls component

---
+++ src/Controls.svelte
@@ -0,0 +1,23 @@
+<style>
+    .fas {
+        font-size: 30px;
+        color: rgb(129, 129, 129);
+        cursor: pointer;
+        user-select: none;
+    }
+    .fas:hover{
+        filter: brightness(80%);
+    }
+    .main-button {
+        font-size: 40px;
+        position: relative;
+        top: 3px;
+        margin: 0 1rem;
+    }
+</style>
+
+<div class="player-controls">
+    <i class="fas fa-backward" id="prev" title="Previous" />
+    <i class="fas fa-play main-button" id="play" title="Play" />
+    <i class="fas fa-forward" id="next" title="Next" />
+</div>
